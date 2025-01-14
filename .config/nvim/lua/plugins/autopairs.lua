55a9ee7267da337c39b986e0c07f90fbb18cb35d
tokisuno <lucaswmei@gmail.com>
2025-01-14 17:24:21 -0500

    ignored chars for autopairs

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -5,6 +5,7 @@ return {
     opts = {
       disable_in_visualblock = false,
       disable_in_replace_mode = true,
+      ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
       map_cr = true,
       map_bs = true,
       map_c_h = false,
