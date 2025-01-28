12e418de5145ecd8c55b84cacd3c556f16e872fa
tokisuno <lucaswmei@gmail.com>
2025-01-28 10:08:22 -0500

    colourscheme priority

--- .config/nvim/lua/plugins/colours.lua
+++ .config/nvim/lua/plugins/colours.lua
@@ -1,6 +1,7 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
+    priority = 1000,
     config = function()
       require("gruvbox").setup({
         terminal_colors = true, -- add neovim terminal colors
