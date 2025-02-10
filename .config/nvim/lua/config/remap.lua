a350a8dc5f5dc91e7a00fea81c1e2840abfe4e9e
tokisuno <lucaswmei@gmail.com>
2025-02-10 07:20:42 -0500

    nvim changes

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -8,12 +8,6 @@ vim.g.maplocalleader = ","
 map("n", "<leader>bn", vim.cmd.bdelete, { desc = "Buffer quit" })
 map("n", "<leader>bv", vim.cmd.bunload, { desc = "Buffer unload" })
 
--- Disable arrow keys in all modes
--- map('', '<up>',    '<nop>')
--- map('', '<down>',  '<nop>')
--- map('', '<left>',  '<nop>')
--- map('', '<right>', '<nop>')
-
 map("n", "<C-p>", ":bprev<cr>", opts)
 map("n", "<C-n>", ":bnext<cr>", opts)
 
@@ -35,12 +29,3 @@ map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 map('n', '<leader>fq', builtin.quickfix, { desc = 'Telescope quickfix' })
-
--- trouble.nvim
-map('n', '<leader>d', "<cmd>echo 'this is for trouble later'<cr>")
-map('n', "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
-map('n', "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "Buffer Diagnostics (Trouble)" })
-map('n', "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols (Trouble)" })
-map('n', "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", { desc = "LSP Definitions / references / ... (Trouble)" })
-map('n', "<leader>xL", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List (Trouble)" })
-map('n', "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
