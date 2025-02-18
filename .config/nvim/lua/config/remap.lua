93a89dbc07bac80c4916354d0c81a49fed03ce70
tokisuno <lucaswmei@gmail.com>
2025-02-18 09:48:01 -0500

    font size, remaps, tmux

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -30,8 +30,8 @@ map("n", "<leader>m", ":Mason<cr>", { noremap = true, silent = true, desc = "Ope
 
 -- telescope
 map('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
-map('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope document symbols' })
 map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 map('n', '<leader>fq', builtin.quickfix, { desc = 'Telescope quickfix' })
+map('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'Telescope document symbols' })
