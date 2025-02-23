9d2031cde0f99a25e73f716ce00916581ae93c13
tokisuno <lucaswmei@gmail.com>
2025-02-23 14:10:38 -0500

    cleaning up nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,14 +7,12 @@ local set     = vim.opt
 local autocmd = vim.api.nvim_create_autocmd
 
 vim.o.background     = "dark"
--- vim.cmd.colorscheme("kanagawa-wave")
 vim.cmd.colorscheme("gruvbox")
 vim.g.have_nerd_font = true
 set.termguicolors = true
 set.guicursor     = ""
 set.cursorline    = true
 
--- wildmenu --
 set.wildmode   = "list:longest"
 set.wildignore = {'*.docx','*.jpg','*.png','*.gif','*.pdf','*.pyc','*.exe','*.flv','*.img','*.xlsx'}
 
@@ -32,16 +30,10 @@ vim.diagnostic.config({
   },
 })
 
--- number column --
 set.nu             = true
 set.relativenumber = true
 set.signcolumn     = "number"
--- vvv needs further testing vvv
--- set.numberwidth=3
--- set.signcolumn="yes:1"
--- set.statuscolumn="%l%s"
 
--- backups --
 set.swapfile = false
 vim.o.backup = false
 set.undofile = true
@@ -49,7 +41,6 @@ set.undodir  = os.getenv("HOME") .. "/.vim/undodir"
 set.isfname:append("@-@")
 set.undofile = true
 
--- formatting --
 set.tabstop     = 4
 set.shiftwidth  = 4
 set.softtabstop = 4
@@ -83,32 +74,17 @@ autocmd('FileType', {
     end,
 })
 
-vim.api.nvim_create_user_command("FormatDisable", function(args)
-  if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
-    vim.b.disable_autoformat = true
-  else
-    vim.g.disable_autoformat = true
-  end
-end, { desc = "Disable autoformat-on-save", bang = true, })
-
-vim.api.nvim_create_user_command("FormatEnable", function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
-end, { desc = "Re-enable autoformat-on-save", })
-
 -- going to try replacing with confirm.nvim
--- autocmd("BufWritePre", {
---     pattern = {"*"},
---     callback = function(ev)
---         save_cursor = vim.fn.getpos(".")
---         vim.cmd([[%s/\s\+$//e]])
---         vim.fn.setpos(".", save_cursor)
---     end,
--- })
+autocmd("BufWritePre", {
+    pattern = {"*"},
+    callback = function(ev)
+        save_cursor = vim.fn.getpos(".")
+        vim.cmd([[%s/\s\+$//e]])
+        vim.fn.setpos(".", save_cursor)
+    end,
+})
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-
 autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
