b4f7ddefe4dcbcaf82d7dcb6ae4bfa674510f565
tokisuno <lucaswmei@gmail.com>
2025-04-24 16:06:23 -0400

    i have to go back to kitty (ghostty copr doesn't work idk why)

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-sudo dnf install neovim
-sudo dnf install emacs
-sudo dnf install awesome
-sudo dnf install kitty
-sudo dnf install tmux
-sudo dnf install fish
+# to add: essential packages that i use
+# sudo dnf install neovim
+# sudo dnf install emacs
+# sudo dnf install awesome
+# sudo dnf install kitty
+# sudo dnf install tmux
+# sudo dnf install fish
+
+# this is for jetbrainsmono nerd font
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
+&& cd ~/.local/share/fonts \
+&& unzip JetBrainsMono.zip \
+&& rm JetBrainsMono.zip \
+&& fc-cache -fv
