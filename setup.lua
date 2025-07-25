d930fbb58a5aa088c5a60e37307a821334f50ccf
tokisuno <lucaswmei@gmail.com>
2025-07-25 09:34:00 -0400

    made a little lua script to install basic things on new installation of fedora

---
+++ setup.lua
@@ -0,0 +1,31 @@
+-- NOTE: This is only for my personal use on Fedora 42
+-- If you want to adapt this to your own distro, I've made it easy
+-- by making the package manager a variable you can change
+PkgManager = " dnf install -y "
+local packages = {
+	"kitty",
+	"awesome",
+	"emacs",
+	"zsh",
+	"neovim",
+	"tmux",
+	"git",
+	"lazygit",
+	"fastfetch",
+	"flameshot",
+	"flatpak",
+}
+
+for _, app in pairs(packages) do
+	print("Now attempting to install: " .. app)
+	os.execute("sudo" .. PkgManager .. app)
+end
+
+print("This is just in case flatpak doesn't auto-add its own repo")
+os.execute("flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo")
+
+-- This is Fedora specific
+print("Controversially installing Google Chrome")
+os.execute("sudo" .. PkgManager .. "fedora-workstation-repositories")
+os.execute("sudo dnf config-manager setopt google-chrome.enabled=1")
+os.execute("sudo" .. PkgManager .. "google-chrome-stable")
