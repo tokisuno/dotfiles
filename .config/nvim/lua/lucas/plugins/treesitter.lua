6f7d92312550da781367a8721d4ecffd274320ef
tokisuno <lucaswmei@gmail.com>
2025-08-06 12:49:20 -0400

    context plugin, tmux remaps

--- .config/nvim/lua/lucas/plugins/treesitter.lua
+++ .config/nvim/lua/lucas/plugins/treesitter.lua
@@ -1,32 +1,37 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  config = function ()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        "c",
-        "cpp",
-        "css",
-        "gitignore",
-        "html",
-        "javascript",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "ruby",
-        "rust",
-        "toml",
-        "vim",
-        "vimdoc",
-        "yaml",
-        "zig",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    })
-  end
+  {
+    'nvim-treesitter/nvim-treesitter',
+    config = function ()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          "c",
+          "cpp",
+          "css",
+          "gitignore",
+          "html",
+          "javascript",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "ruby",
+          "rust",
+          "toml",
+          "vim",
+          "vimdoc",
+          "yaml",
+          "zig",
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context'
+  }
 }
