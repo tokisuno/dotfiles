0eb58a13358116566b237866b70b62032c357e8e
tokisuno <lucaswmei@gmail.com>
2025-08-25 13:12:22 -0400

    (qtile): removed some comments, fixed formatting on lambda for bar

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -78,24 +78,18 @@ groups = [Group(i) for i in "123456789"]
 for i in groups:
     keys.extend(
         [
-            # mod + group number = switch to group
             Key(
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
                 desc=f"Switch to group {i.name}",
             ),
-            # mod + shift + group number = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
                 desc=f"Switch to & move focused window to group {i.name}",
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod + shift + group number = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
@@ -124,14 +118,14 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        top=bar.Bar(
+        bottom=bar.Bar(
             [
                 widget.TextBox(":3", name="default"),
                 # widget.CurrentLayout(),
                 widget.GroupBox(
                     border_width=2,
                     disable_drag=2,
-                    highlight_method="line",
+                    highlight_method="block",
                     highlight_color=["#000000", "#000000"],
                     spacing=0
                     ),
@@ -151,7 +145,7 @@ screens = [
                     name='minutesleft',
                     update_interval=60,
                     fmt='(Only {} minutes left!)',
-                    func = lambda: subprocess.check_output(
+                    func=lambda: subprocess.check_output(
                         os.path.expanduser('~/code/builds/goltime/bin/goltime')
                         ).decode('utf-8').strip(),
                     ),
@@ -204,16 +198,7 @@ wl_input_rules = None
 wl_xcursor_theme = None
 wl_xcursor_size = 24
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
-
 
 @hook.subscribe.startup_once
 def autostart():
