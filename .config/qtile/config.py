1705f5048101bb3ced4849c845a47d6aeb05d0c3
tokisuno <lucaswmei@gmail.com>
2025-11-23 15:13:44 -0500

    (qtile): fix the autostart for the last fkn time

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -201,19 +201,7 @@ wl_xcursor_size = 24
 
 @hook.subscribe.startup_once
 def autostart_once():
-    cmds = [
-            "nitrogen --restore"
-            "ibus-daemon -rxRd"
-            "nm-applet"
-            "flameshot"
-            "syncthing"
-            "pactl set-default-sink alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo"
-            "pactl set-sink-volume @DEFAULT_SINK@ 25%"
-            "xinput set-prop 'Logitech ERGO M575' 'Coordinate Transformation Matrix' 0.6, 0, 0, 0, 0.6, 0, 0, 0, 0.6"
-            "xinput set-prop 'Logitech ERGO M575' 'libinput Accel Speed' 2"
-            ]
-    for cmd in cmds:
-        subprocess.run(cmd)
+    subprocess.Popen(f"{Path("~/code/repos/dotfiles/.config/qtile").expanduser()}/autostart.sh")
     Keyboard()
 
 wmname = "LG3D"
